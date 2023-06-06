3fc1d13ec4530a07b0dbb42c443f6e49c2a25245
stvnsy <stevenjakeasoy>
2023-06-06 09:20:57 +0800

    Adding all files task tracker

---
+++ vue-tasktracker/src/App.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="container">
+    <Header  title="Task tracker"/>
+    <Tasks @toggle-reminder="toggleReminder"
+    @delete-task="deleteTask" :tasks="tasks" />
+  </div>
+  
+</template>
+
+<script>
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Tasks,
+  },
+  data() {
+    return {
+      tasks: []
+    }
+  },
+  methods: {
+    deleteTask(id) {
+      if(confirm('Are you sure you want to delete?')) {
+        this.tasks = this.tasks.filter((task) => task.id !== id)
+      }
+    },
+    toggleReminder(id) {
+      this.tasks = this.tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder}
+      : task)
+    },
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'March 1 at 2:30pm',
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'March 3 at 1:30pm',
+        reminder: false,
+      },
+      {
+        id: 3,
+        text: 'Dinner out',
+        day: 'March 7 at 6:30pm',
+        reminder: true,
+      },
+    ]
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: 'Poppins', sans-serif;
+}
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+.btn:focus {
+  outline: none;
+}
+.btn:active {
+  transform: scale(0.98);
+}
+.btn-block {
+  display: block;
+  width: 100%;
+}
+</style>
